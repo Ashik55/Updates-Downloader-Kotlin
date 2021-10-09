2f8701ed6a8774c7780340676528e5fd510b5d4e
Md. Ashikur Rahman <38959435+Ashik55@users.noreply.github.com>
2021-10-09 07:03:23 +0600

    init

---
+++ app/src/main/java/com/aashiq/updatedownloader/MainActivity.kt
@@ -0,0 +1,256 @@
+package com.aashiq.updatedownloader
+
+import android.annotation.SuppressLint
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import android.widget.Toast
+import java.io.File
+
+
+import android.content.ActivityNotFoundException
+
+import androidx.core.content.FileProvider
+
+import android.content.ContentResolver
+
+import android.webkit.MimeTypeMap
+
+import android.app.Activity
+import android.database.Cursor
+import java.lang.IllegalStateException
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var downloadBtn : Button
+    val URL = "https://objectstorage.ap-mumbai-1.oraclecloud.com/n/bm1xnhkwswfs/b/bucket-db/o/app-debug.apk"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         downloadBtn = findViewById(R.id.downloadBtn)
+
+        downloadBtn.setOnClickListener {
+
+            downloadApp("https://objectstorage.ap-mumbai-1.oraclecloud.com/n/bm1xnhkwswfs/b/bucket-db/o/app-debug.apk")
+            //downloadFile(this@MainActivity, URL, "app-debug.apk")
+
+        }
+
+
+
+    }
+
+
+    private fun downloadApp(downloadUrl: String?) {
+
+
+
+        Toast.makeText(applicationContext, "Downloading. . . ",Toast.LENGTH_LONG).show()
+
+
+        var destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/"
+        val fileName = "myapp.apk"
+        destination += fileName
+
+
+        val uri: Uri = Uri.parse("file://$destination")
+
+
+        //Delete update file if exists
+
+        //Delete update file if exists
+        val file = File(destination)
+        if (file.exists()) //file.delete() - test this, I think sometimes it doesnt work
+            file.delete()
+
+
+
+
+        //set downloadmanager
+        val request = DownloadManager.Request(Uri.parse(downloadUrl))
+        request.setDescription("Downloading Sindabad Halkhata App")
+        request.setTitle("Sindabad Halkhata")
+
+
+        //set destination
+        request.setDestinationUri(uri)
+
+        // get download service and enqueue file
+        val manager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadId = manager.enqueue(request)
+
+        //set BroadcastReceiver to install app when .apk is downloaded
+        val onComplete: BroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(ctxt: Context, intent: Intent?) {
+
+                val action = intent?.action
+                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE == action) {
+                    val downloadId = intent.getLongExtra(
+                        DownloadManager.EXTRA_DOWNLOAD_ID, 0
+                    )
+                    openDownloadedAttachment(ctxt, downloadId)
+                }
+            }
+        }
+        //register receiver for when .apk download is compete
+        registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+    }
+
+
+    /**
+     * Used to download the file from url.
+     *
+     *
+     * 1. Download the file using Download Manager.
+     *
+     * @param url      Url.
+     * @param fileName File Name.
+     */
+    fun downloadFile(activity: Activity, url: String?, fileName: String?) {
+        try {
+            if (url != null && !url.isEmpty()) {
+                val uri = Uri.parse(url)
+                activity.registerReceiver(
+                    attachmentDownloadCompleteReceive, IntentFilter(
+                        DownloadManager.ACTION_DOWNLOAD_COMPLETE
+                    )
+                )
+                val request = DownloadManager.Request(uri)
+                request.setMimeType(getMimeType(uri.toString()))
+                request.setTitle(fileName)
+                request.setDescription("Downloading attachment..")
+                request.allowScanningByMediaScanner()
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+                val dm = activity.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                dm.enqueue(request)
+            }
+        } catch (e: IllegalStateException) {
+            Toast.makeText(
+                activity,
+                "Please insert an SD card to download file",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    /**
+     * Used to get MimeType from url.
+     *
+     * @param url Url.
+     * @return Mime Type for the given url.
+     */
+    private fun getMimeType(url: String): String? {
+        var type: String? = null
+        val extension = MimeTypeMap.getFileExtensionFromUrl(url)
+        if (extension != null) {
+            val mime = MimeTypeMap.getSingleton()
+            type = mime.getMimeTypeFromExtension(extension)
+        }
+        return type
+    }
+
+    /**
+     * Attachment download complete receiver.
+     *
+     *
+     * 1. Receiver gets called once attachment download completed.
+     * 2. Open the downloaded file.
+     */
+    var attachmentDownloadCompleteReceive: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE == action) {
+                val downloadId = intent.getLongExtra(
+                    DownloadManager.EXTRA_DOWNLOAD_ID, 0
+                )
+                openDownloadedAttachment(context, downloadId)
+            }
+        }
+    }
+
+    /**
+     * Used to open the downloaded attachment.
+     *
+     * @param context    Content.
+     * @param downloadId Id of the downloaded file to open.
+     */
+
+    @SuppressLint("Range")
+    private fun openDownloadedAttachment(context: Context, downloadId: Long) {
+        val downloadManager = context.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val query = DownloadManager.Query()
+        query.setFilterById(downloadId)
+        val cursor: Cursor = downloadManager.query(query)
+        if (cursor.moveToFirst()) {
+            val downloadStatus: Int =
+                cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+            val downloadLocalUri: String =
+                cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
+            val downloadMimeType: String =
+                cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE))
+            if (downloadStatus == DownloadManager.STATUS_SUCCESSFUL && downloadLocalUri != null) {
+
+
+
+                openDownloadedAttachment(context, Uri.parse(downloadLocalUri), downloadMimeType)
+            }
+        }
+        cursor.close()
+    }
+
+    /**
+     * Used to open the downloaded attachment.
+     *
+     *
+     * 1. Fire intent to open download file using external application.
+     *
+     * 2. Note:
+     * 2.a. We can't share fileUri directly to other application (because we will get FileUriExposedException from Android7.0).
+     * 2.b. Hence we can only share content uri with other application.
+     * 2.c. We must have declared FileProvider in manifest.
+     * 2.c. Refer - https://developer.android.com/reference/android/support/v4/content/FileProvider.html
+     *
+     * @param context            Context.
+     * @param attachmentUri      Uri of the downloaded attachment to be opened.
+     * @param attachmentMimeType MimeType of the downloaded attachment.
+     */
+    private fun openDownloadedAttachment(
+        context: Context,
+        attachmentUri: Uri,
+        attachmentMimeType: String
+    ) {
+        var attachmentUri: Uri? = attachmentUri
+        if (attachmentUri != null) {
+            // Get Content Uri.
+            if (ContentResolver.SCHEME_FILE == attachmentUri.scheme) {
+                // FileUri - Convert it to contentUri.
+                val file = File(attachmentUri.path)
+                attachmentUri = FileProvider.getUriForFile(this@MainActivity, "com.aashiq.updatedownloader.provider", file)
+            }
+            val openAttachmentIntent = Intent(Intent.ACTION_VIEW)
+            openAttachmentIntent.setDataAndType(attachmentUri, attachmentMimeType)
+            openAttachmentIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            try {
+                context.startActivity(openAttachmentIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    context,
+                    context.getString(R.string.unable_to_open_file),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+}
